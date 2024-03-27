81d8422df83958bcdb23fdbf3d8d0607a89a3a2a
shvmm0032 <shvmbutola@gmail.com>
2024-03-27 07:10:32 -0700

    lay foundation or react

--- App.js
+++ App.js
@@ -1,50 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-//NOTE: now u see this its very complicated that nested tag for multiple type of tags iam using  it is very confusing right thats why we use JSX for the react ..
+//React Element//
+const elem = <span>react element inside element . </span>
+const heading = <h1>
+    {elem}
+    It is a react element  which is calling inside the react functional component.</h1>
+//React Component//
+const Title = () =>(<h1 className="head">Hello Shivam its another functional componets</h1>);
 
-//Nested tag//
+//REACT FUNCTIONAL COMPONENTS//
 
-// <div id="parent">
-//     <div id="child">
-//         <h1>i am h1 tag</h1>
-//         <h2>i am h1 tag</h2>
-//     </div>
-//     <div id="child2">
-//         <h1>i am h1 tag</h1>
-//         <h2>i am h1 tag</h2>
-//     </div>
-// </div>
-
-//Example of this//
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [
-    React.createElement(
-        "div",
-        { id: "child" },
-        [React.createElement(
-            "h1",
-            {}, "iam h1 tag"),
-        React.createElement(
-            "h2",
-            {}, "iam h2 tag")]
-    ),
-    //2//
-    React.createElement(
-        "div",
-        { id: "child2" },
-        [React.createElement(
-            "h1",
-            {}, "iam h1 tag"),
-        React.createElement(
-            "h2",
-            {}, "iam h2 tag")]
-    )
-    ]
+const name ="shivam" ;
+const number =10000;
+const HeadingComponent = () => (
+  <div id="container">
+    <Title/>
+    {heading}
+    <h2>{name}</h2>
+    <h2>{number}</h2>
+    <h2>{100+400}</h2>
+    {console.log("osho Danu")}
+    <h1 className="heading">Hello Shivam its functonal components</h1>
+  </div>
 );
-console.log(parent);
 
-//simle tag//
-// const heading = React.createElement("h1", {}, "Hello React How are you!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>);
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nameste-react
+
+# Parcel
+- HMR-Hot Module Replacement(when we save the file it automatically refresh the page )
